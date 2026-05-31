4965c5a34d6e0fcce42b7094d1228e40f6af024b
Olivier Chavarin <memoire1@gmail.com>
2026-05-31 18:03:17 +0200

    fix: revised range.

--- scripts/development/serviceWorker.js
+++ scripts/development/serviceWorker.js
@@ -80,7 +80,7 @@
  *   navigator.serviceWorker.addEventListener('message', handler)
  */
 
-const CACHE_NAME = 'v105'
+const CACHE_NAME = 'v106'
 const MEDIA_CACHE_NAME = `media-${CACHE_NAME}`
 const OFFLINE_URL = '/offline.html'
 
